c9b65552f5d31dbcbb3fc82c805e109a3f455ee5
conan1152 <conan1152@gmail.com>
2022-05-04 12:57:42 +0700

    init source

---
+++ index.js
@@ -0,0 +1,48 @@
+const { launch, getStream } = require("puppeteer-stream");
+const fs = require("fs");
+
+const file = fs.createWriteStream(__dirname + "/test.webm");
+const file2 = fs.createWriteStream(__dirname + "/test2.webm");
+
+async function test() {
+
+    browser = await launch({
+        args: ['--headless=chrome'],
+    })
+
+    const page = await browser.newPage();
+    await page.goto("https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1");
+    const stream = await getStream(page, { audio: true, video: true });
+    console.log("recording");
+
+    stream.pipe(file);
+    setTimeout(async () => {
+        await stream.destroy();
+        file.close();
+        await page.close();
+        console.log("finished");
+    }, 1000000 * 10);
+}
+
+async function test2() {
+
+    browser = await launch({
+        args: ['--headless=chrome'],
+    })
+
+    const page = await browser.newPage();
+    await page.goto("https://www.youtube.com/embed/zpmtdQiFaIo?autoplay=1");
+    const stream = await getStream(page, { audio: true, video: true });
+    console.log("recording");
+
+    stream.pipe(file2);
+    setTimeout(async () => {
+        await stream.destroy();
+        file2.close();
+        await page.close();
+        console.log("finished");
+    }, 1000000 * 10);
+}
+
+test();
+test2();
